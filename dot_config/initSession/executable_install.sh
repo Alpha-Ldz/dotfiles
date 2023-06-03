be2ec50e985161833f73005dd5a3940cffce769c
Pierre-Louis <pierre-louis.landouzi@sopht.com>
2023-06-03 19:51:22 +0200

    UPDATE packages list

--- dot_config/initSession/executable_install.sh
+++ dot_config/initSession/executable_install.sh
@@ -1,5 +1,7 @@
 #/bin/ssh
 
+sudo pacman -S ~/.config/initSession/packages.txt
+
 (cd ~/.local/share/chezmoi/ && git remote remove origin)
 (cd ~/.local/share/chezmoi/ && git remote add origin git@github.com:Alpha-Ldz/dotfiles.git)
 
@@ -10,15 +12,15 @@ mkdir ~/Documents/Git
 
 (cd ~/Documents/Git/yay && makepkg -si )
 
-yay -S --needed --noconfirm - < ~/.config/initSession/packages.txt
+yay -S --needed --noconfirm - < ~/.config/initSession/packagesYay.txt
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sudo systemctl enable ly.service preload.service
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm -rf ~/.zshrc
 mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
-
-sudo systemctl enable ly.service preload.service
 
 sudo reboot
