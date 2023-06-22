67baf721935c89077d17da2bdbab71a82b487d17
Your Name <you@example.com>
2023-06-22 20:39:14 +0200

    hyprland add files

---
+++ dot_config/hypr/tmp.py
@@ -0,0 +1,35 @@
+def test():
+    # Matteo is very gay
+    print("matteo is very gay")
+
+
+
+def test1():
+    # Matteo is very gay
+    print("matteo is very gay")
+
+
+
+def test2():
+    # Matteo is very gay
+    print("matteo is very gay")
+
+
+
+def test3():
+    # Matteo is very gay
+    print("matteo is very gay")
+
+
+
+def test4():
+    # Matteo is very gay
+    print("matteo is very gay")
+
+
+
+def test5():
+    # Matteo is very gay
+    print("matteo is very gay")
+
+
