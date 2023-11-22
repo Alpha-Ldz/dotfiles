ceabad7236a21f638422558c4397986040e5c25b
Pierre-Louis <pllandouzi@gmail.com>
2023-11-22 21:36:25 +0100

    Add zsh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
-
-
-
-
-
 rm -rf ~/.config/hypr && mv ./config/hypr ~/.config/
 mv ./config/* ~/.config/
 git reset --hard
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
