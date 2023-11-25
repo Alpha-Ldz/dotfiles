212bfb67e52679d2a66157b823df4bdeda180160
Pierre-Louis <pllandouzi@gmail.com>
2023-11-25 14:29:55 +0100

    Update setup

--- setup.sh
+++ setup.sh
@@ -4,8 +4,16 @@ mv zshrc ../.zshrc
 git reset --hard
 
 ssh-keygen -t rsa -b 4096
+git config --global user.email "pllandouzi@gmail.com"
+git config --global user.name "Pierre-Louis"
+git remote remove origin
+git remote add origin git@github.com:Alpha-Ldz/dotfiles.git
 
 sudo pacman -Syu rofi neovim kitty zsh npm unzip fd ripgrep tree python-pip jq virt-manager virt-viewer qemu vde2 ebtables iptables-nft nftables dnsmasq bridge-utils ovmf swtpm linux-firmware-qlogic
+
+git clone https://aur.archlinux.org/yay.git
+cd yay && makepkg -si
+cd .. && rm -rf yay
 
 yay -S --needed --noconfirm swww thorium-browser-bin ttf-firacode-nerd wd719x-firmware upd72020x-fw aic94xx-firmware 
 
